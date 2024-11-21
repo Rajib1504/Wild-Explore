33ffe47ceffbeb29663cf8b8452ea9dcd3b2874b
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-21 17:02:58 +0530

    Created a responsive 404 error page using HTML and Tailwind CSS

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,24 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
     <div>
-      <body class="bg-white flex items-center justify-center min-h-screen">
+      <body class="bg-[#f0f7fad7] flex items-center justify-center min-h-screen">
         <div class="text-center">
-          <h1 class="text-9xl font-extrabold text-black leading-tight">404</h1>
+          <div className="flex gap-2 justify-center ml-4 items-center">
+            <div className="text-6xl">😫</div>
+            <div className="text-6xl">😫</div>
+          </div>
+          <h1 class="text-9xl font-extrabold text-pink-200 leading-tight">
+            404
+          </h1>
           <p class="text-2xl font-medium text-gray-600 mb-5">
             Oops! The page you are looking for does not exist.
           </p>
-          <p class="text-lg text-gray-500 mb-6">
+          <p class="text-lg text-gray-500 mb-6 ">
             It might have been moved or deleted.
           </p>
-          <a
-            href="/"
-            class="text-lg bg-yellow-400 text-white px-6 py-2 rounded-lg font-semibold hover:bg-yellow-300"
+          <NavLink
+            to={"/"}
+            class="text-lg bg-blue-400 text-white px-6 py-4 rounded-lg font-semibold hover:bg-blue-300"
           >
             Back to Home
-          </a>
+          </NavLink>
         </div>
       </body>
     </div>
