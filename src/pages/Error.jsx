bb0ad2eada7479bada363c68f149ef57639c07c8
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-21 16:48:07 +0530

    Added custom 404 error page with user-friendly design and navigation button

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,18 +3,24 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="bg-[#f56f6056] w-full min-h-screen">
-      <div>
-        <div className="flex justify-center items-center flex-col gap-2 pt-40">
-          <h1 className="text-6xl font-extrabold text-white  text-center">
-            404
-          </h1>
-          <p className="text-white text-center ">You are in wrong path </p>
-          <Link to={"/"} className="btn btn-wide bg-blue-200 text-white">
-            Go Back
-          </Link>
+    <div>
+      <body class="bg-white flex items-center justify-center min-h-screen">
+        <div class="text-center">
+          <h1 class="text-9xl font-extrabold text-black leading-tight">404</h1>
+          <p class="text-2xl font-medium text-gray-600 mb-5">
+            Oops! The page you are looking for does not exist.
+          </p>
+          <p class="text-lg text-gray-500 mb-6">
+            It might have been moved or deleted.
+          </p>
+          <a
+            href="/"
+            class="text-lg bg-yellow-400 text-white px-6 py-2 rounded-lg font-semibold hover:bg-yellow-300"
+          >
+            Back to Home
+          </a>
         </div>
-      </div>
+      </body>
     </div>
   );
 };
