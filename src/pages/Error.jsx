bbfa4dca0a4190d827958ae844ab63c492fe0e59
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-21 17:10:39 +0530

    Added a Back to Home button for easy navigation to the homepage

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,24 +4,24 @@ import { Link, NavLink } from "react-router-dom";
 const Error = () => {
   return (
     <div>
-      <body class="bg-[#f0f7fad7] flex items-center justify-center min-h-screen">
-        <div class="text-center">
+      <body className="bg-[#f0f7fad7] flex items-center justify-center min-h-screen">
+        <div className="text-center">
           <div className="flex gap-2 justify-center ml-4 items-center">
             <div className="text-6xl">😫</div>
             <div className="text-6xl">😫</div>
           </div>
-          <h1 class="text-9xl font-extrabold text-pink-200 leading-tight">
+          <h1 className="text-9xl font-extrabold text-pink-200 leading-tight">
             404
           </h1>
-          <p class="text-2xl font-medium text-gray-600 mb-5">
+          <p className="text-2xl font-medium text-gray-600 mb-5">
             Oops! The page you are looking for does not exist.
           </p>
-          <p class="text-lg text-gray-500 mb-6 ">
+          <p className="text-lg text-gray-500 mb-6 ">
             It might have been moved or deleted.
           </p>
           <NavLink
             to={"/"}
-            class="text-lg bg-blue-400 text-white px-6 py-4 rounded-lg font-semibold hover:bg-blue-300"
+            className="text-lg bg-blue-400 text-white px-6 py-4 rounded-lg font-semibold hover:bg-blue-300"
           >
             Back to Home
           </NavLink>
